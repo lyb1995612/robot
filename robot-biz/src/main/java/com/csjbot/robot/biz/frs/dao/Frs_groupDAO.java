62ba1ffc1d7204f81c6edf837e65d38758afd97c
zhangyangyang <944943563@qq.com>
2017-05-04 13:11:19 +0800

    人脸识别迁移   -zyy  170504130802

---
+++ robot-biz/src/main/java/com/csjbot/robot/biz/frs/dao/Frs_groupDAO.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.csjbot.robot.biz.frs.dao;
+
+
+import com.csjbot.robot.base.dao.BaseDAO;
+import com.csjbot.robot.biz.frs.model.Frs_group;
+
+/**
+ * @author 作者：Zhangyangyang
+ * @version 创建时间：2017年3月27日 上午9:54:51
+ * 类说明
+ */
+public interface Frs_groupDAO extends BaseDAO<Frs_group> {
+	
+	//根据group_id查询group
+	public abstract Frs_group findGroupByGroupId(String group_id);
+}
