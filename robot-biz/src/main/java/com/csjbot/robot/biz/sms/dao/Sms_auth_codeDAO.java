fcb029b3df6bbe0ac2f17b139ecd79da24270693
zhangyangyang <944943563@qq.com>
2017-05-05 11:39:55 +0800

    小雪后台管理模块迁移  zyy  170505114023

---
+++ robot-biz/src/main/java/com/csjbot/robot/biz/sms/dao/Sms_auth_codeDAO.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.csjbot.robot.biz.sms.dao;
+
+
+import com.csjbot.robot.base.dao.BaseDAO;
+import com.csjbot.robot.biz.sms.model.Sms_auth_code;
+
+/**
+ * @author 作者：Zhangyangyang
+ * @version 创建时间：2017年3月27日 下午4:40:18
+ * 类说明
+ */
+public interface Sms_auth_codeDAO extends BaseDAO<Sms_auth_code> {
+	//根据账号查询验证码
+	public abstract Sms_auth_code findCodeByAccount(String account);
+}
---
+++ robot-biz/src/main/java/com/csjbot/robot/biz/sms/dao/Sms_secretDAO.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.csjbot.robot.biz.sms.dao;
+
+
+import com.csjbot.robot.base.dao.BaseDAO;
+import com.csjbot.robot.biz.sms.model.Sms_secret;
+
+/**
+ * @author 作者：Zhangyangyang
+ * @version 创建时间：2017年3月27日 下午4:43:04
+ * 类说明
+ */
+public interface Sms_secretDAO extends BaseDAO<Sms_secret> {
+	//根据账号获得密钥
+	public abstract Sms_secret findSecretByAccount(String account);
+}
